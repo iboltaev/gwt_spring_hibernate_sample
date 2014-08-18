fb3769280c019c6b6adbf0070545467cc163fb14
Ilya Boltaev <boltaev@gmail.com>
2014-08-18 14:44:08 +0400

    Update DataChangeListener.java

--- src/main/java/ru/naumen/knowledgebase/client/DataChangeListener.java
+++ src/main/java/ru/naumen/knowledgebase/client/DataChangeListener.java
@@ -2,6 +2,9 @@ package ru.naumen.knowledgebase.client;
 
 import ru.naumen.knowledgebase.client.json.*;
 
+/**
+ * Interface for data update events
+ * */
 public interface DataChangeListener {
     public enum Action { EDIT, ADD, DELETE };
 
